bc0c21df938ead051adfe7d48991118f9d9ba212
ThalesEsteves <105940869+ThalesEsteves@users.noreply.github.com>
2023-02-09 14:49:36 -0300

    Add files via upload

---
+++ Web (PI)/Login/Contato.js
@@ -0,0 +1,7 @@
+const nome = document.getElementById("nome")
+
+const sobrenome = document.getElementById("sobrenome")
+
+const email = document.getElementById("email")
+
+const mensagem = document.getElementById("mensagem")
